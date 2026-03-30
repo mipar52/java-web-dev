4b1edceb35a0dacb0d6d8c08dae8ca3f559e2d6d
mparadina <milanparadina83@gmail.com>
2026-03-30 18:27:24 +0200

    add gitignore, initial spring project

---
+++ video-game-store/src/main/java/com/milan/videogamestore/VideoGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.milan.videogamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoGameStoreApplication.class, args);
+    }
+
+}
---
+++ video-game-store/src/test/java/com/milan/videogamestore/VideoGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milan.videogamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
