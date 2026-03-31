91eaf2cb0dd1f456e9f955872636f396748c9591
mparadina <milanparadina83@gmail.com>
2026-03-31 13:12:18 +0200

    create order entities

---
+++ video-game-store/src/main/java/com/milan/videogamestore/model/orders/Order.java
@@ -0,0 +1,47 @@
+package com.milan.videogamestore.model.orders;
+
+import com.milan.videogamestore.model.users.AppUser;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="app_user_id", nullable = false)
+    private AppUser user;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private String paymentMethod;
+
+    @Column(nullable = false)
+    private String shippingAddress;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String zip;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items = new ArrayList<>();
+}
