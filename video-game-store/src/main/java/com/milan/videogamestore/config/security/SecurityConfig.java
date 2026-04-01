5b7c4855cd7aa6f1eb357f07de16ba10e4026173
mparadina <milanparadina83@gmail.com>
2026-04-01 13:12:31 +0200

    update sec config with log entries

--- video-game-store/src/main/java/com/milan/videogamestore/config/security/SecurityConfig.java
+++ video-game-store/src/main/java/com/milan/videogamestore/config/security/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.milan.videogamestore.config.security;
 
+import com.milan.videogamestore.config.security.logEntries.LogAuthFailureHandler;
+import com.milan.videogamestore.config.security.logEntries.LogAuthSuccessHandler;
+import com.milan.videogamestore.repository.LogEntryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,7 +12,10 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final LogEntryRepository logEntryRepository;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -16,14 +23,19 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/register", "/login").permitAll()
+                        .requestMatchers("/media/**").permitAll()
                         .requestMatchers("/games/**", "/genres/**", "/cart/**").permitAll()
                         .requestMatchers("/checkout/**", "/orders/**").authenticated()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
                 .formLogin(form ->
                         form
                                 .loginPage("/login")
                                 .defaultSuccessUrl("/games", true)
+                                .successHandler(new LogAuthSuccessHandler(logEntryRepository))
+                                .failureHandler(new LogAuthFailureHandler(logEntryRepository))
+                                .permitAll()
                                 .permitAll())
                 .logout(Customizer.withDefaults())
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
