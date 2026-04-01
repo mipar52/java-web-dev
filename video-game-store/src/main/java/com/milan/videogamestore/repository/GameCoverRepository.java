a84f61701257dede460fa06c857d68204785552c
mparadina <milanparadina83@gmail.com>
2026-04-01 13:14:51 +0200

    add missing game cover repo

---
+++ video-game-store/src/main/java/com/milan/videogamestore/repository/GameCoverRepository.java
@@ -0,0 +1,6 @@
+package com.milan.videogamestore.repository;
+
+import com.milan.videogamestore.model.game.GameCover;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameCoverRepository extends JpaRepository<GameCover, Long> { }
