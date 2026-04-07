417e8cee96ae06b6ca665a6df7703254fe40c360
mparadina <milanparadina83@gmail.com>
2026-04-07 13:17:59 +0200

    add home controller to redirect to games

---
+++ video-game-store/src/main/java/com/milan/videogamestore/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.milan.videogamestore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String justGoHome() {
+        return "redirect:/games";
+    }
+}
